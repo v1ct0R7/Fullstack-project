6f3ab0d3e7615d93c13252b59b82f134653fad49
Elvis Muratigit config --global user.email elvismurati15@gmail.com <98538952+v1ct0R7@users.noreply.github.com>
2026-09-25 02:09:26 +0200

    complete router get byId

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const {Posts} = require('../models')
+const express = require('express');
+const router = express.Router();
+const {Posts} = require('../models');
 
 router.get("/", async (req, res ) => {
     const listofPosts = await Posts.findAll();
     res.json(listofPosts);
+});
+
+router.get("/byId/:id", async (req, res) => {
+    const id = req.params.id;
+    const post = await Posts.findByPk(id);
+    res.json(post);
 });
 
 router.post('/', async (req, res) => {
